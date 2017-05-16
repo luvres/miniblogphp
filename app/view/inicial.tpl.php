35e3bdf0714f733549e6b622503d8d655b7d1924
luvres <luvres@hotmail.com>
2017-05-16 20:17:21 -0300

    Posts

--- app/view/inicial.tpl.php
+++ app/view/inicial.tpl.php
@@ -5,12 +5,17 @@
 <?php foreach($tpl["inicial"]["posts"] as $post) { ?>
 
   	<div class="blog-post">
+			<!-- Título do Post -->
 			<h2 class="blog-post-title"><?=$post["titulo"]?></h2>
+			<!-- Autor -->
 			<p class="blog-post-meta">Autor: <?=$post['nome']?></p>
+			<!-- Conteúdo - Texto -->
 			<p><pre><?=$post['texto']?></pre></p>
+			<!-- Editar Post -->
 			<a href="index.php?m=updatePost" onclick="" >
 	    	<i class="glyphicon glyphicon-edit" style="color:#428bca"></i>
 	    </a>
+			<!-- Excluir Post -->
 			<a href="index.php?m=deletePost" onclick="return confirm('Excluir Usuario?')" >
 	      <i class="glyphicon glyphicon-trash" style="color:#fa5858"></i>
 	    </a>
--- app/view/site.tpl.php
+++ app/view/site.tpl.php
@@ -17,7 +17,7 @@
   <div class="container">
     <div class="blog-header">
       <h1 class="blog-title">Mini Blog PHP</h1>
-      <p class="lead blog-description">Template of creating a blog with Bootstrap.</p>
+      <p class="lead blog-description">Modelo de criação de um mini blog com o Bootstrap.</p>
       <hr>
     </div>
 
